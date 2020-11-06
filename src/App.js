c4b36145b9fe98370b3b1c6135817913dbab80f1
c_melchor <cah.melchor@gmail.com>
2020-11-06 08:49:41 -0800

    axios request fulfilled

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import Character from "./components/Character";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [characters, setCharacters] = useState(" ");
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  useEffect(() => {
+    axios
+      .get("https://swapi.dev/api/people")
+      .then(res => {
+        // console.log(res.data.results);
+        const charactersArray = res.data.results;
+        setCharacters(charactersArray);
+      })
+      .catch(err => {
+        console.log(err, "NOPE, PLEASE FIX THIS");
+      });
+  }, []);
+
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
+  console.log(characters, "hello here i am");
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <div>
+        <h2>HELLO TEST TEST</h2>
+        <Character people={characters} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,23 @@
 // Write your Character component here
+
+import React from "react";
+
+//need to install and import styled components
+
+export default function Character(props) {
+  //   const { name, height } = props;
+  console.log(props, "characters");
+
+  return (
+    <div>
+      {/* {props.map(item => {
+        return (
+          <div>
+            Name : {item.name}
+            height:{item.height}
+          </div>
+        );
+      })} */}
+    </div>
+  );
+}
